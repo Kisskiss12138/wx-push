2ca831c5cc7d802775ffbac2ed69b8992161f810
Kisskiss12138 <142304698+Kisskiss12138@users.noreply.github.com>
2023-08-15 17:32:25 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,9 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx8af114fef3ae196e",
+  appsecret: "91a22ec53f9c171e81905b24d04da28d",
+  template_id: "ydhoQRRGMbcgipcmgb6qMag7xjzDPFWX_BnJ8Tz0aEY",
+  touser: "osbtl6pj_AeoEZaYqhFLlLtfX_Vw",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "成都",
 };
